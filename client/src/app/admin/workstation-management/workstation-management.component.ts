d779bc9b41aaa1c1a18255688bedfd63348acd6c
Brandenn28 <yeechenliaw@gmail.com>
2025-02-03 03:48:36 +0800

    Changes

---
+++ client/src/app/admin/workstation-management/workstation-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-workstation-management',
+  imports: [],
+  templateUrl: './workstation-management.component.html',
+  styleUrl: './workstation-management.component.css'
+})
+export class WorkstationManagementComponent {
+
+}
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -10,12 +10,20 @@ export const routes: Routes = [{
     pathMatch: 'full',
     loadComponent: ()=>
         import('./pages/product-list/product-list.component').then(m=>m.ProductListComponent)
-    // component: ProductListComponent
 },
 {
     path:'cart',
     loadComponent: ()=>
         import('./pages/cart/cart.component').then(m=>m.CartComponent)
-    // component: CartComponent
+},
+{
+    path:'admin/workstation-management',
+    loadComponent: ()=>
+        import('./admin/workstation-management/workstation-management.component').then(m=>m.WorkstationManagementComponent)
+},  
+{
+    path:'admin',
+    loadComponent: ()=>
+        import('./admin/dashboard/dashboard.component').then(m=>m.DashboardComponent)
 },
 ];
