8c9464d20b65a2adf7149c495faf7006733bfb94
Brandenn28 <yeechenliaw@gmail.com>
2025-02-05 04:19:40 +0800

    admin view

--- client/src/app/admin/workstation-management/workstation-management.component.ts
+++ client/src/app/admin/workstation-management/workstation-management.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { Dialog } from 'primeng/dialog';
+import { InputTextModule} from 'primeng/inputtext';
 
 @Component({
   selector: 'app-workstation-management',
-  imports: [TableModule, CommonModule],
+  imports: [TableModule, CommonModule, ToolbarModule, ButtonModule, Dialog, InputTextModule],
   templateUrl: './workstation-management.component.html',
   styleUrl: './workstation-management.component.css'
 })
 export class WorkstationManagementComponent {
+
+  NewBtnDialog: boolean = false;
+
+  showNewBtnDialog(){
+    this.NewBtnDialog = true;
+  }
+
+
 
   product:any[] = [];
 
@@ -16,5 +28,6 @@ export class WorkstationManagementComponent {
     const res = await fetch('https://fakestoreapi.com/products/category/electronics')
     const data = await res.json();
     this.product = data;
+    this.NewBtnDialog = true;
   }
 }
