14ca685aa292f571ec3aa19fb593b7a448871362
Brandenn28 <yeechenliaw@gmail.com>
2025-03-17 21:59:41 +0700

    changes

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   
+  // DI
+  fb = inject(FormBuilder)
+
+  // Properties
+  LoginForm!:FormGroup;
+
+  constructor(private auth:AuthService,){
+    this.LoginForm = this.fb.group({
+      email:["", Validators.required],
+      password:["", Validators.required],
+    });
+  }
+
+  LoginButton(){
+    console.log(this.LoginForm.value);
+  }
+
+
+
+
 
 }
